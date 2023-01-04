96455c02484ecca037a0178fb99b9241567cb5d5
sadjokerwin <al107ivanov@gmail.com>
2023-01-04 02:11:35 +0200

    Update TravelListNode.cpp

--- Projects/Project_1/TravelListNode.cpp
+++ Projects/Project_1/TravelListNode.cpp
@@ -1,6 +1,8 @@
-// #include "TravelListNode.h"
+#include "TravelListNode.h"
 // TravelListNode::TravelListNode(const string &name, TravelListNode *next)
 // {
 //     mName = name;
 //     mNext = next;
 // }
+// void TravelListNode::print()
+
